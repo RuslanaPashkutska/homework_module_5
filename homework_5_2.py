fd453da718f109521584078f091692abba393864
RuslanaPashkutska <ryslana.pashkytska@gmail.com>
2025-02-22 15:35:55 +0100

    update

--- homework_5_2.py
+++ homework_5_2.py
@@ -6,8 +6,8 @@ def generator_numbers(text: str) -> Generator[float, None, None]:
     """
     Generator that finds all real numbers in the text and returns then as float values.
     """
-    for match in re.finditer(r'\b\d+\.\d+\b|\b\d+\b', text):
-        yield float(match.group())
+    for match in re.finditer(r' (\d+\.\d+|\d+) ', text):
+        yield float(match.group(1))
 
 def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]) -> float:
     """
